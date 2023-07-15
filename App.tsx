95ecbb9d6955a02c4ae45d5ef275b9f4f7ca4388
hennadii919 <yenenkohennadii919@gmail.com>
2023-07-16 02:45:08 +0530

    layout add

--- App.tsx
+++ App.tsx
@@ -1,30 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { Box, Flex, NativeBaseProvider, Text, extendTheme } from 'native-base';
+import { NativeBaseProvider } from 'native-base';
 import React from 'react';
 import {
   StyleSheet,
 } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-const newColorTheme = {
-  brand: {
-    900: "#8287af",
-    800: "#7c83db",
-    700: "#b3bef6",
-  },
-};
-const theme = extendTheme({ colors: newColorTheme });
+import { themes } from './src/constants';
 
 function App(): JSX.Element {
-  return <NativeBaseProvider theme={theme}>
-    <SafeAreaProvider>
-    </SafeAreaProvider>
+  return <NativeBaseProvider isSSR={false} theme={themes}>
   </NativeBaseProvider>
 }
 
---
+++ src/constants/theme.ts
@@ -0,0 +1,22 @@
+import { extendTheme } from 'native-base';
+
+export default extendTheme({
+	fontConfig: {
+		Satoshi: {
+			400: {
+				normal: 'Satoshi-Regular'
+			},
+			500: {
+				normal: 'Satoshi-Medium'
+			},
+			700: {
+				normal: 'Satoshi-Bold'
+			}
+		}
+	},
+	fonts: {
+		heading: 'Satoshi',
+		body: 'Satoshi',
+		mono: 'Satoshi'
+	}
+});
