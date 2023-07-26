65272cdf12fa7407da452ef9d097e72c2c686078
hennadii919 <yenenkohennadii919@gmail.com>
2023-07-27 01:25:20 +0530

    work on welcome icon

--- src/modules/auth/welcome/index.tsx
+++ src/modules/auth/welcome/index.tsx
@@ -1,4 +1,4 @@
-import {Box, Image, StatusBar} from 'native-base';
+import {ArrowForwardIcon, Box, IconButton, Image, StatusBar} from 'native-base';
 import React from 'react';
 import {images} from '../../../constants';
 
@@ -12,7 +12,23 @@ const Welcome = () => {
         alt={'splash'}
         source={images.splash_img}
       />
-      <Box height={'100%'} safeAreaY width="100%" position={'absolute'} />
+      <Box
+        height={'100%'}
+        justifyContent={'flex-end'}
+        alignItems={'center'}
+        safeAreaY
+        width="100%"
+        position={'absolute'}>
+        <IconButton
+          backgroundColor={'#0F2432'}
+          borderRadius={30}
+          variant={'solid'}
+          height={50}
+          width={50}
+          mb={6}>
+          <ArrowForwardIcon size={5} color={'white'} />
+        </IconButton>
+      </Box>
     </Box>
   );
 };
